ec1f38927ac0035b12db1ffc487bad3a39cab664
TeaChoco <85914523+TeaChoco@users.noreply.github.com>
2025-12-19 17:35:27 +0700

    Update hospitals.module.ts

--- server/src/api/hospitals/hospitals.module.ts
+++ server/src/api/hospitals/hospitals.module.ts
@@ -8,6 +8,6 @@ import { Hospital, HospitalSchema } from './schemas/hospital.schema';
 @Module({
     imports: [...new ImportsMongoose({ name: Hospital.name, schema: HospitalSchema }).imports],
     providers: [HospitalsService],
-    controllers: [HospitalsController],
+    // controllers: [HospitalsController],
 })
 export class HospitalsModule {}
