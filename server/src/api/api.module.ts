106e3a7472baceba6136dbd072de2e776cbc16ac
TeaChoco <85914523+TeaChoco@users.noreply.github.com>
2025-12-19 17:31:10 +0700

    commit

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,6 +1,6 @@
 //- Path: "TeaChoco-Hospital/server/src/api/api.module.ts"
 import { Module } from '@nestjs/common';
-import { SocketModule } from './socket/socket.module';
+// import { SocketModule } from './socket/socket.module';
 import { DoctorsModule } from './doctors/doctors.module';
 import { MedicinesModule } from './medicines/medicines.module';
 import { HospitalsModule } from './hospitals/hospitals.module';
