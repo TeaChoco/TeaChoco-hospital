346fb93f53e436fda0fee66843b5d566da9ad595
TeaChoco <85914523+TeaChoco@users.noreply.github.com>
2025-12-22 08:52:40 +0700

    Update hospitals.module.ts

--- server/src/api/hospitals/hospitals.module.ts
+++ server/src/api/hospitals/hospitals.module.ts
@@ -6,7 +6,7 @@ import { HospitalsController } from './hospitals.controller';
 import { Hospital, HospitalSchema } from './schemas/hospital.schema';
 
 @Module({
-    imports: [...new ImportsMongoose({ name: Hospital.name, schema: HospitalSchema }).imports],
+    // imports: [...new ImportsMongoose({ name: Hospital.name, schema: HospitalSchema }).imports],
     // providers: [HospitalsService],
     // controllers: [HospitalsController],
 })
