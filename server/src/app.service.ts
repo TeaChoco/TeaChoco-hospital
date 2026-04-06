52c80179e6d0bf120c844e902a7e34cda6d6882a
TeaChoco <thannateetibmerit@gmail.com>
2026-04-06 08:56:58 +0700

    commit

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,4 +1,5 @@
 //-Path: "TeaChoco-Hospital/server/src/app.service.ts"
+import packageJson from '../package.json';
 import { Injectable } from '@nestjs/common';
 import { SecureService } from './secure/secure.service';
 
@@ -7,7 +8,7 @@ export class AppService {
     constructor(private readonly secureService: SecureService) {}
 
     getHello(): object {
-        return { isDev: this.secureService.isDev() };
+        return { isDev: this.secureService.isDev(), version: packageJson.version };
     }
 
     getAllEnv(): Record<string, string> {
